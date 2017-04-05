7ee7254093dc06d1a2c76e69719accc26e833a61
paarthlakhani <paarth.lakhani@utah.edu>
2017-04-04 17:46:27 -0700

    Added some useful comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :pet_event, polymorphic: true
-  belongs_to :pet, optional: true
+  belongs_to :pet, optional: true # this is an  optional column, lost dog owner might not know which dog it is.
   belongs_to :user
 end
