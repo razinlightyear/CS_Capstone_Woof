08d2a1c0354560a2dd0f2bb790f0a0c07075f30d
Andrew Emrazian <razinlightyear@gmail.com>
2017-04-04 20:02:21 -0600

    Add a page for around me events for a user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :around_mes, except: :show
+  get ':around_mes/:user_id' => 'around_mes#show'
   resources :colors
   resources :weights
   resources :breeds
@@ -6,6 +8,6 @@ Rails.application.routes.draw do
   resources :groups
   resources :users
   
-  root 'home#index' # create a homepage that doeasn't belong to a view
+  root 'home#index' # create a homepage that doeasn't belong to a model
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
