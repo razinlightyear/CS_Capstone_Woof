4b48bcb84766be254195c09c51e175721f53e73e
paarthlakhani <paarth.lakhani@utah.edu>
2017-08-30 21:42:36 -0700

    Made a few changes long time back

--- app/models/found_dog.rb
+++ app/models/found_dog.rb
@@ -6,14 +6,12 @@ class FoundDog < ApplicationRecord
   belongs_to :weight
 end
 
-
 # ruby determine class 
 # @paarth[3].pet_event.around_me_event.is_a?FoundDog
 
 # most of the type, we are going to expect right away.
-# you can view latitue and longitude in lostdog but you cannot set it in lost dog.
+# you can view latitude and longitude in lostdog but you cannot set it in lost dog.
 # we have forwaded the values to lost dog
-#  if we want to change the values of latitude and longitude, that needs to be 
-#in around_me
+#  if we want to change the values of latitude and longitude, that needs to be in around_me
 
-# when creatig the marker, you have to also ceate the form with a unique identifier. pop overs on google maps.
+# when creating the marker, you have to also ceate the form with a unique identifier. pop overs on google maps.
