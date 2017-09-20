5a169b9de497ee606f6c6319ebb2a653eb4e425f
Andrew Emrazian <razinlightyear@gmail.com>
2017-09-19 23:41:49 -0600

    User email shouldn't be blank.

--- db/schema.rb
+++ db/schema.rb
@@ -106,19 +106,19 @@ ActiveRecord::Schema.define(version: 20170918135009) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",                                   null: false
-    t.datetime "updated_at",                                   null: false
-    t.string   "encrypted_password",              default: "", null: false
-    t.integer  "sign_in_count",                   default: 0,  null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "encrypted_password",              default: "",   null: false
+    t.integer  "sign_in_count",                   default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "email",                           default: "", null: false
+    t.string   "email",                                          null: false
     t.datetime "remember_created_at"
     t.string   "authentication_token", limit: 30
+    t.boolean  "active",                          default: true, null: false
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
-    t.boolean  "active",          default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
