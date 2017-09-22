7fe7d8a06cb9bf3644f5fbf8177b9c54a3777221
Andrew Emrazian <razinlightyear@gmail.com>
2017-09-21 23:35:17 -0600

    Add model validations so that we can use the messages in the forms.

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,6 +1,8 @@
 class Color < ApplicationRecord
-  has_and_belongs_to_many :pets
+  has_and_belongs_to_many :pets, inverse_of: :colors
   #has_and_belongs_to_many :lost_dogs
-  
+    
+  validates :name, presence: { message: "Please enter a color name" }
+    
   scope :contains, -> (name) {where("name LIKE ?", "%#{name}%")}
 end
