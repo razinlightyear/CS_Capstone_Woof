ff218ea3e6636ba1cb47b05d31a1720a69509be4
paarthlakhani <paarth.lakhani@utah.edu>
2017-09-24 17:23:26 -0700

    Removed certain fields from the database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
   acts_as_token_authentication_handler_for User, fallback: :none
+  #before_action :authenticate_user!, except: [:home_index]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :password_confirmation])
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :timeoutable
+
+=begin        
+Database Authenticable
+Registerable
+Rememberable
+Timeoutable
+=end
 
   has_many :owns_groups, class_name: "Group", foreign_key: "owner_id"
   has_and_belongs_to_many :groups
