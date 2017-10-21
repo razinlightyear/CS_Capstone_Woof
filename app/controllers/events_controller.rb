dc921070130dc28ea4a0c8f384b7c5ba8d014e3b
paarthlakhani <paarth.lakhani@utah.edu>
2017-10-21 14:16:02 -0700

    Users would be able to view, create, edit and destroy lost dog and found dog events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,33 +1,14 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:edit, :update, :destroy]
+
+
+
   def index
     @events = Event.around_me.includes(:user,pet: [:colors,:breed,:weight])
   end
 
   def show
-  #@user = User.find(params[:user_id]) # The user that is "logged in"
-    @user = User.find(current_user) # The user that is "logged in"
-    @pets = Pet.where(group: @user.groups).includes(:breed,:weight,:colors) # The "logged in" user's pets (so they can create a lost dog event)
-    
-    if !@pets.empty?
-      @around_me_events = Event.where(is_around_me: true).includes(:user,pet: [:colors,:breed,:weight])
-    else
-      @around_me_events = Array.new
-    end
-
-    @around_me_locations = Array.new();
-    @descriptions = Array.new();
-    
-    @around_me_events.each do |around_me|
-      @around_me_locations.push(around_me)
-      @descriptions.push(around_me.description)
-    end
-    gon.around_me_events = @around_me_locations
-    gon.event_types = @around_me_locations.map {|e| e.model_name.human }
-    gon.descriptions = @descriptions
-    
-    return @around_me_events
-
+    @events = Event.all
   end
   
   private
