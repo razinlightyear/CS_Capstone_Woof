3b053673884791f30d24881bf8ce0776a9e44377
paarthlakhani <paarth.lakhani@utah.edu>
2017-11-11 22:17:43 -0800

    Users are able to edit found dog event addresses

--- app/controllers/found_dogs_controller.rb
+++ app/controllers/found_dogs_controller.rb
@@ -40,16 +40,30 @@ class FoundDogsController < ApplicationController
     end
 
     def update
+
         respond_to do |format|
-            if @found_dog.update(found_dog_params)
-                format.html { redirect_to event_path(current_user), notice: 'Found Dog event has been updated' }
-                # for doing the JSON reply
-                format.json { render :show, status: :ok }
+            if @found_dog.address != params[:found_dog][:address]
+                if @found_dog.update(found_dog_params)
+                    format.js
+                    # for doing the JSON reply
+                    format.json { render :show, status: :ok }
+                else
+                    format.html { render :edit }
+                    format.json { render json: found_dog.errors, status: :unprocessable_entity }     
+                end
             else
-                format.html { render :edit }
-                format.json { render json: found_dog.errors, status: :unprocessable_entity }     
-            end 
+                if @found_dog.update(found_dog_params)
+                    format.html { redirect_to event_path(current_user), notice: 'Found Dog event has been updated' }
+                    # for doing the JSON reply
+                    format.json { render :show, status: :ok }
+                else
+                    format.html { render :edit }
+                    format.json { render json: found_dog.errors, status: :unprocessable_entity }     
+                end
+            end
         end
+
+        
     end
 
     # DELETE /found_dogs/1
